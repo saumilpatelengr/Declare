e924b6d623528138b7b38ed3ee829e504bfb8c9f
Saumil Patel <saumilp16@gmail.com>
2026-06-24 14:00:00 -0500

    Comments added to card.py

--- src/card.py
+++ src/card.py
@@ -1,13 +1,18 @@
+#Card class
 class Card:
     #Creates a standard playing card with its rank and suit
     def __init__(self, suit, rank):
         self._suit = suit
         self._rank = rank
 
+
+
     #Compares if 2 cards are the exact same
     def __eq__(self, other):
         return self._suit == other.suit and self._rank == other.rank
     
+
+
     #Checks the rank of a card and returns the point value of that card
     def value(self):
         if self._rank == 'K' or self._rank == 'Q' or self._rank == 'J':
@@ -16,11 +21,15 @@ class Card:
             return 1
         else:
             return int(self._rank)
-        
+
+
+
     #Returns the card's rank
     @property
     def rank(self):
         return self._rank
+    
+
     
     #Returns the card's suit
     @property
