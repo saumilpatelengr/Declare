8424e9a55389f1e94558521fc83c6bdfb8dacc26
Saumil Patel <saumilp16@gmail.com>
2026-07-09 19:28:48 -0500

    Added comments to paths.py

--- src/paths.py
+++ src/paths.py
@@ -1,10 +1,18 @@
+#Imports
 import os
 import sys
 
+
+
+#Returns the absolute path to a file both before and after packaging
 def resource_path(relative):
+    #Check if the program is running as a packaged executable
     if getattr(sys, "frozen", False):
+        #Uses temporary folder
         base = sys._MEIPASS
     else:
+        #Uses the project's root folder
         base = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+    #Returns the full path to the file
     return os.path.join(base, relative)
